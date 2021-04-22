9d33bbd77cd65f4ae54cab05388dc1090764b6e1
Dan Davison <dandavison7@gmail.com>
2021-04-22 08:53:29 -0400

    WIP

--- src/views/changes/hunkView.ts
+++ src/views/changes/hunkView.ts
@@ -1,3 +1,5 @@
+import { html } from 'diff2html';
+
 import { MagitChangeHunk } from '../../models/magitChangeHunk';
 import { Section } from '../general/sectionHeader';
 import { TextView } from '../general/textView';
@@ -5,9 +7,11 @@ import { TextView } from '../general/textView';
 export class HunkView extends TextView {
   isFoldable = true;
 
-  get id() { return this.changeHunk.diff; }
+  get id() {
+    return this.changeHunk.diff;
+  }
 
   constructor(public section: Section, public changeHunk: MagitChangeHunk) {
-    super(changeHunk.diff);
+    super(html(changeHunk.diff));
   }
 }
