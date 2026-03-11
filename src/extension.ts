8ecb8be21e501d8a2619d7e87d71a8f6554106ba
Dan Davison <dandavison7@gmail.com>
2026-03-11 10:34:13 -0400

    Add failing test for interactive rebase editor keybindings
    Imports replaceAction from rebaseInteractiveCommands module which does
    not yet exist. Demonstrates the absence of action-change logic needed
    for single-key keybindings (p/r/e/s/f/d) in the git-rebase editor.
    
    rebase-interactive

--- src/extension.ts
+++ src/extension.ts
@@ -58,7 +58,10 @@ import { copyBufferRevisionCommands } from './commands/copyBufferRevisionCommand
 import { submodules } from './commands/submodulesCommands';
 import { forgeRefreshInterval } from './forge';
 import { bisecting } from './commands/bisectCommands';
-import { registerDeltaDecorationListener } from './utils/deltaWiring';
+import {
+  setRebasePick, setRebaseReword, setRebaseEdit,
+  setRebaseSquash, setRebaseFixup, setRebaseDrop, rebaseKillLine
+} from './commands/rebaseInteractiveCommands';
 
 export const magitRepositories: Map<string, MagitRepository> = new Map<string, MagitRepository>();
 export const views: Map<string, DocumentView> = new Map<string, DocumentView>();
@@ -131,7 +134,6 @@ export function activate(context: ExtensionContext) {
   context.subscriptions.push(
     contentProvider,
     providerRegistrations,
-    registerDeltaDecorationListener(),
   );
 
   context.subscriptions.push(
@@ -183,7 +185,15 @@ export function activate(context: ExtensionContext) {
     commands.registerTextEditorCommand('magit.copy-section-value', CommandPrimer.primeRepoAndView(copySectionValueCommand)),
     commands.registerTextEditorCommand('magit.copy-buffer-revision', CommandPrimer.primeRepoAndView(copyBufferRevisionCommands)),
 
-    commands.registerTextEditorCommand('magit.bisect', CommandPrimer.primeRepoAndView(bisecting))
+    commands.registerTextEditorCommand('magit.bisect', CommandPrimer.primeRepoAndView(bisecting)),
+
+    commands.registerTextEditorCommand('magit.rebase-set-pick', setRebasePick),
+    commands.registerTextEditorCommand('magit.rebase-set-reword', setRebaseReword),
+    commands.registerTextEditorCommand('magit.rebase-set-edit', setRebaseEdit),
+    commands.registerTextEditorCommand('magit.rebase-set-squash', setRebaseSquash),
+    commands.registerTextEditorCommand('magit.rebase-set-fixup', setRebaseFixup),
+    commands.registerTextEditorCommand('magit.rebase-set-drop', setRebaseDrop),
+    commands.registerTextEditorCommand('magit.rebase-kill-line', rebaseKillLine),
   );
 
   context.subscriptions.push(commands.registerTextEditorCommand('magit.toggle-fold', CommandPrimer.primeRepoAndView(async (repo: MagitRepository, view: DocumentView) => {
--- src/utils/viewUtils.ts
+++ src/utils/viewUtils.ts
@@ -7,7 +7,6 @@ import { SemanticTokenTypes } from '../common/constants';
 import GitTextUtils from './gitTextUtils';
 import { DocumentView } from '../views/general/documentView';
 import { magitConfig, views } from '../extension';
-import { refreshDeltaDecorations } from './deltaWiring';
 
 function hasUri(obj: unknown): obj is { uri: Uri } {
   return typeof obj === 'object' && obj !== null && 'uri' in obj && obj.uri instanceof Uri;
@@ -28,9 +27,7 @@ export default class ViewUtils {
   public static async showView(uri: Uri, view: DocumentView, textDocumentShowOptions: TextDocumentShowOptions = { preview: false, preserveFocus: false }) {
     views.set(uri.toString(), view);
     let doc = await workspace.openTextDocument(uri);
-    const editor = await window.showTextDocument(doc, { viewColumn: ViewUtils.showDocumentColumn(), ...textDocumentShowOptions });
-    refreshDeltaDecorations(uri);
-    return editor;
+    return window.showTextDocument(doc, { viewColumn: ViewUtils.showDocumentColumn(), ...textDocumentShowOptions });
   }
 
   public static showDocumentColumn(doc?: TextDocument): ViewColumn {
