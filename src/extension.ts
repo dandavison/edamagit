de6ffb33b6ba49e0814f51b96f15f26f5813b191
Dan Davison <dandavison7@gmail.com>
2026-03-11 20:52:07 -0400

    Add VS Code settings for delta executable and syntax theme

--- src/extension.ts
+++ src/extension.ts
@@ -70,7 +70,7 @@ export const processLog: MagitProcessLogEntry[] = [];
 
 export let gitApi: API;
 export let logPath: string;
-export let magitConfig: { displayBufferSameColumn?: boolean, forgeEnabled?: boolean, hiddenStatusSections: Set<string>, quickSwitchEnabled?: boolean, gitPath?: string };
+export let magitConfig: { displayBufferSameColumn?: boolean, forgeEnabled?: boolean, hiddenStatusSections: Set<string>, quickSwitchEnabled?: boolean, gitPath?: string, deltaExecutable?: string, deltaSyntaxTheme?: string };
 
 function loadConfig() {
   let workspaceConfig = workspace.getConfiguration('magit');
@@ -80,7 +80,9 @@ function loadConfig() {
     forgeEnabled: workspaceConfig.get('forge-enabled'),
     hiddenStatusSections: readHiddenStatusSections(workspaceConfig.get('hide-status-sections')),
     quickSwitchEnabled: workspaceConfig.get('quick-switch-enabled'),
-    gitPath: workspaceConfig.get('git-path')
+    gitPath: workspaceConfig.get('git-path'),
+    deltaExecutable: workspaceConfig.get('delta-executable') || undefined,
+    deltaSyntaxTheme: workspaceConfig.get('delta-syntax-theme') || undefined,
   };
 
   let configCodePath: string | undefined = workspaceConfig.get('code-path');
