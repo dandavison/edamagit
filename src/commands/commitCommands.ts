a92b2a2df2fee96d09e453793056ec08b1d9eece
Dan Davison <dandavison7@gmail.com>
2026-03-19 14:34:38 -0400

    Fix staged diff view not being closed after commit
    The cleanup code compared Uri objects by reference (===) which could
    fail, and used showDocumentColumn() to compute a target column that
    might differ from where the editor actually was. Replace the
    show-then-close dance with the tab API to find and close the tab
    by URI string match.

--- src/commands/commitCommands.ts
+++ src/commands/commitCommands.ts
@@ -10,9 +10,7 @@ import { Section } from '../views/general/sectionHeader';
 import * as path from 'path';
 import FilePathUtils from '../utils/filePathUtils';
 import * as fs from 'fs';
-import ViewUtils from '../utils/viewUtils';
 import GitTextUtils from '../utils/gitTextUtils';
-import { magitConfig } from '../extension';
 
 const commitMenu = {
   title: 'Committing',
@@ -188,14 +186,11 @@ export async function runCommitLikeCommand(repository: MagitRepository, args: st
 
     const stagedEditor = await stagedEditorTask;
     if (stagedEditor) {
-      for (const visibleEditor of vscode.window.visibleTextEditors) {
-        if (visibleEditor.document.uri === stagedEditor.document.uri) {
-          // This is a bit of a hack. Too bad about editor.hide() and editor.show() being deprecated.
-          const stagedEditorViewColumn = ViewUtils.showDocumentColumn();
-          await vscode.window.showTextDocument(stagedEditor.document, { viewColumn: stagedEditorViewColumn, preview: false });
-          await vscode.commands.executeCommand('workbench.action.closeActiveEditor');
-          if (!magitConfig.displayBufferSameColumn) {
-            vscode.commands.executeCommand(`workbench.action.navigate${stagedEditorViewColumn === vscode.ViewColumn.One ? 'Right' : 'Left'}`);
+      const targetUri = stagedEditor.document.uri.toString();
+      for (const tabGroup of vscode.window.tabGroups.all) {
+        for (const tab of tabGroup.tabs) {
+          if (tab.input instanceof vscode.TabInputText && tab.input.uri.toString() === targetUri) {
+            await vscode.window.tabGroups.close(tab);
           }
         }
       }
